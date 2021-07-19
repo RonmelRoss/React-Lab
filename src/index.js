d0975fb049b225b3d80cd93ed4fbb4cb1406ef20
Ronmel Ross <ronmelross@gmail.com>
2021-07-20 00:19:41 +0800

    [#8] Make Component Interactive Using States

--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ import './App.css';
 import './index.css';
 
 class Square extends React.Component {
+    constructor (props) {
+      super(props);
+      this.state = {
+        value: null,
+      }
+    }
+
     render() {
       return (
-        <button className="square">
-          {/* TODO */}
-          { this.props.value }
+        <button className="square" onClick={() => this.setState({value: 'X'})}>
+          { this.state.value }
         </button>
       );
     }

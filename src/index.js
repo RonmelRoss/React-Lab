99d96f11e5dc14e737807723b0aac9a87968667b
Ronmel Ross <ronmelross@gmail.com>
2021-07-19 23:00:49 +0800

    [#7] Pass Prop From Parent To Child

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ class Square extends React.Component {
       return (
         <button className="square">
           {/* TODO */}
+          { this.props.value }
         </button>
       );
     }
@@ -15,7 +16,7 @@ class Square extends React.Component {
   
   class Board extends React.Component {
     renderSquare(i) {
-      return <Square />;
+      return <Square value={i} />;
     }
   
     render() {
